ce4a7542f4894c9d1c203e4742b5f5556080128e
Rogier Git <n0pr1v4cy@gmail.com>
2015-02-22 22:52:20 +0100

    second

--- ui.R
+++ ui.R
@@ -1,9 +1,9 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-        headerPanel("Iris variable analysis"),
+        headerPanel("Species classification using iris characteristics"),
         sidebarPanel(
                 h2('Parameters'),
-                p('test'),   
+                p('Please select a variable for the X-axis and the Y-axis. The plot will be drawn when 2 different variables are selected.'),   
                 h3('X-axis'),
                 p('Select a field for the X-axis'),
         
@@ -32,6 +32,9 @@ shinyUI(pageWithSidebar(
         ),
         
         mainPanel(
+                h3('Introduction'),
+                p('This shiny app will allow you to explore the iris dataset from R. The dataset contains 4 iris characteristics that can be used to specify a species. In this app you can select a variable for X-axis and for the Y-axis. The plot will then show these 2 variables and colorcode the species.'),
+                
                 h3('Results'),
                 
                 plotOutput("Plot"),
